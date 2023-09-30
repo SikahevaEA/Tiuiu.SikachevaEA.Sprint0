9655b0d88c6e85095000ef4286fb4b4e9e12841e
Tiuiu.SikachovaEA@outlook.com <Tiuiu.SikachovaEA@outlook.com>
2023-09-30 20:38:22 +0500

    Task6

---
+++ Tiuiu.SikachevaEA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tiuiu.SikachevaEA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tiuiu.SikachevaEA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("876b7f1c-ecfe-42ae-867b-8571ec280014")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
